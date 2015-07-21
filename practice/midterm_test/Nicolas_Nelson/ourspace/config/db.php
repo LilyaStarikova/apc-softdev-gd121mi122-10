8ed9c27a82550c628030f14420f3ac7b60af941a
LilyaStarikova <ilyapetrenko13@gmail.com>
2015-07-21 13:07:16 +0800

    mod

--- practice/midterm_test/Nicolas_Nelson/ourspace/config/db.php
+++ practice/midterm_test/Nicolas_Nelson/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_nicolas_nelson_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
